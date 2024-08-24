e0844f87b951401563edd31c0973a4f04c559f28
rajbabbar <rajrathva9499@gmail.com>
2024-08-25 03:09:17 +0530

    connected

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,9 @@ export const PLACEHOLDER_PFP =
 export const PLACEHOLDER_BANNER =
   "https://www.kudratikahumbo.com/wp-content/uploads/2019/12/Hero-Banner-Placeholder-Light-1024x480-1.png";
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://loop-backend-2.vercel.app";
+// export const BASE_URL = "https://loop-backend-2.vercel.app";
 // export const BASE_URL = "https://loop-jfzv.onrender.com"
 // export const BASE_URL = "https://loop-backend-production.up.railway.app"
 
-export const CLIENT_BASE_URL = "https://loop-connect.vercel.app";
+export const CLIENT_BASE_URL = "https://quicktweet-frontend.vercel.app";
+export const SERVER_BASE_URL = "https://quicktweet-backend.vercel.app";
